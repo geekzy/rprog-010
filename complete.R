b18388e572165b6fb0edc5810bc013f9e1f4555f
Ade Imam Kurniawan <ade.imam@samsung.com>
2015-01-13 14:05:54 +0700

    fix complete.R to loop though input id

--- complete.R
+++ complete.R
@@ -3,14 +3,15 @@ complete <- function(directory, id = 1:332) {
     files <- list.files(directory, full.names=TRUE)
     # assuming all files are csv bind all rows together
     data <- do.call(rbind, lapply(files, read.csv))
-    # get subset of within id range
-    data_subset <- data[is.element(data$ID, id),]
-    # find rows with no NA
-    good <- complete.cases(data_subset)
-    # count complete cases of data_subset
-    rows <- data_subset[good,]
-    # split to group by ID
-    s <- split(rows, rows$ID)
-    # count rows based on group
-    data.frame(id=id,nobs=sapply(s, nrow))
+    # prepare output
+    out <- data.frame(id,nobs=0)
+    # loop through id
+    for (i in id) {
+        # count rows with no NA
+        good <- sum(complete.cases(data[which(data$ID == i),]))
+        # put in data.frame
+        out$nobs[id == i] <- good
+    }
+    # return it
+    out
 }

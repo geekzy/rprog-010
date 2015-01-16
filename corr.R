a35d6f58e12469f2b243e5152640e8cfaad32632
Ade Imam Kurniawan <ade.imam@samsung.com>
2015-01-16 17:53:38 +0700

    add assignment 1 part 3 solution code

--- corr.R
+++ corr.R
@@ -1,11 +1,15 @@
 corr <- function(directory, threshold = 0) {
-    # get subset of complete case data
-    cdata <- xcomplete(directory);
-    
-    ## 'threshold' is a numeric vector of length 1 indicating the
-    ## number of completely observed observations (on all
-    ## variables) required to compute the correlation between
-    ## nitrate and sulfate; the default is 0
-    
-    ## Return a numeric vector of correlations
+    # get completely ovserved data
+    cdata <- xcomplete('specdata')
+    out <- numeric(0)
+    for (id in 1:332) {
+        # get subset of complete case data by id
+        xdata <- cdata[which(cdata$ID == id),]
+        # compute correlation between nitrate and sulfate
+        if (nrow(xdata) > threshold) {
+            out <- c(out, cor(xdata$sulfate, xdata$nitrate))
+        }
+    }
+    # return a numeric vector of correlations
+    out
 }

71bea8e7d36130612768b58b3849d5bb758683c9
Ade Imam Kurniawan <ade.imam@samsung.com>
2015-01-26 16:59:23 +0700

    assignment 3 - best hospital

---
+++ best.R
@@ -0,0 +1,37 @@
+best <- function(state, outcome) {
+    # Read outcome data
+    data <- read.csv('outcome-of-care-measures.csv', colClasses = 'character')
+    
+    # Check state and outcome validity
+    vOutcome <- c('heart attack', 'heart failure', 'pneumonia')
+    stateValid <- nrow(subset(data, data[,7] == state)) > 0
+    outcomeValid <- tolower(outcome) %in% vOutcome
+    if (!stateValid) stop('invalid state')
+    if (!outcomeValid) stop('invalid outcome')
+    
+    # Convert to numeric before evaluating
+    data[,13] <- as.numeric(data[,13])
+    data[,19] <- as.numeric(data[,19])
+    data[,25] <- as.numeric(data[,25])
+    
+    # Prepare hospital name
+    hospNames <- ''
+    # Heart Attack evaluation
+    if (tolower(outcome) == 'heart attack') {
+        value <- min(data[which(data[,7] == state),13], na.rm = TRUE)
+        hospNames <- data[which(data[,13] == value),2]
+    }
+    # Heart Failure evaluation
+    else if (tolower(outcome) == 'heart failure') {
+        value <- min(data[which(data[,7] == state),19], na.rm = TRUE)
+        hospNames <- data[which(data[,19] == value),2]
+    }
+    # Pneumonia evaluation
+    else if (tolower(outcome) == 'pneumonia') {
+        value <- min(data[which(data[,7] == state),25], na.rm = TRUE)
+        hospNames <- data[which(data[,25] == value),2]
+    }
+    # Return hospital name
+    # get first aphabetically order hospital name
+    sort(hospNames)[1]
+}

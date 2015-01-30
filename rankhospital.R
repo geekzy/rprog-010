46c5747a618ab0edb44fafd33843aebf5c8896c0
Ade Imam Kurniawan <ade.imam@samsung.com>
2015-01-31 00:41:20 +0700

    fix best.R and start working on rankhospital.R

--- rankhospital.R
+++ rankhospital.R
@@ -1,14 +1,18 @@
-best <- function(state, outcome) {
+rankhospital <- function(state, outcome, num = "best") {
     # Read outcome data
     data <- read.csv('outcome-of-care-measures.csv', colClasses = 'character')
     
     # Check state and outcome validity
+    vNum <- c('best', 'worst')
     vOutcome <- c('heart attack', 'heart failure', 'pneumonia')
     stateValid <- nrow(subset(data, data[,7] == state)) > 0
     outcomeValid <- tolower(outcome) %in% vOutcome
+    numValid <- tolower(num) %in% vNum
     if (!stateValid) stop('invalid state')
     if (!outcomeValid) stop('invalid outcome')
     
+    n <- ifelse(numValid & tolower(num) == 'best', 1, 
+                ifelse(!numValid, as.numeric(num), 0))
     # evaluate which column to use based on specified outcome
     col <- ifelse(tolower(outcome) == 'heart attack', 13, 
                   ifelse(tolower(outcome) == 'heart failure', 19, 
@@ -18,5 +22,10 @@ best <- function(state, outcome) {
     # convert values to numeric
     ds[,col] <- as.numeric(ds[,col])
     # return first ordered by col
-    ds[order(ds[,col], na.last=NA),2][1]
+    orDs <- ds[order(ds[,col], ds[,2], na.last=NA),2]
+    if (numValid & tolower(num) == 'worst')
+        n <- length(orDs)
+    out <- ifelse(n > length(orDs), NA, orDs[n])
+    # return it
+    out
 }
